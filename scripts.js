4ddf916c9dda3d22e22a7ae286cd6aa073bcff0b
ClintonKing <clintonjoking@gmail.com>
2017-05-11 20:48:38 -0400

    Dynamic year range generation

--- scripts.js
+++ scripts.js
@@ -1,105 +1,70 @@
 //Start with selecting the body and creating svg and axes
-var body = d3.select('body');
+var graphDiv = d3.select('div.chartOne');
 
 //Set variables for height and width of svg
-var width = 500;
-var height = 500;
+var width = parseInt(d3.select('div.chartOne').style("width"));
+var height = 400;
 
 //Create SVG
-var svg = body.append('svg')
+var svg = graphDiv.append('svg')
   .attr("width", width)
   .attr("height", height);
-
-var albums;
 
 // var x = d3.time.scale()
 //   .domain([new Date('2009'),new Date('2016')])
 //   .range([0, width]);
 
 
-//Define format for dates
-var format = d3.time.format("%Y-%m-%dT%H:%M:%SZ")
-
 //Variables for number of albums within each year.
-
-var count2009 = 0;
-var count2010 = 0;
-var count2011 = 0;
-var count2012 = 0;
-var count2013 = 0;
-var count2014 = 0;
-var count2015 = 0;
-var count2016 = 0;
-
-var counts;
+var allYears = [];
+var yearSpan = [];
+var countArray = [];
+var counts = [];
 
 //Now let's call our data
-d3.json('albums.json', function(err,data){
+d3.json('chastity.json', function(err,data){
   if (err) throw error;
-  //Assign data to var
-  albums = data;
   //Check to see if our data is coming in
   console.log(data);
 
   //Here's where we actually count the albums in each year for the vars above.
   for(var i = 0; i < data.length; ++i){
-    var release = new Date(data[i].release);
-    var year = release.getFullYear();
-    if(year == 2009){
-      count2009++;
-    } else if(year == 2010){
-      count2010++;
-    } else if(year == 2011){
-      count2011++;
-    } else if(year == 2012){
-      count2012++;
-    } else if(year == 2013){
-      count2013++;
-    } else if(year == 2014){
-      count2014++;
-    } else if(year == 2015){
-      count2015++;
-    } else if (year == 2016){
-      count2016++;
-    }
+    var release = data[i].release;
+    var year = release.substring(0,4);
+    yearInt = Number(year);
+    allYears.push(yearInt);
   };
-  //Let's see if those look right...
+  allYears.sort();
 
-  counts = [
-    {
-      "year": 2009,
-      "albums": count2009
-    },
-    {
-      "year": 2010,
-      "albums": count2010
-    },
-    {
-      "year": 2011,
-      "albums": count2011
-    },
-    {
-      "year": 2012,
-      "albums": count2012
-    },
-    {
-      "year": 2013,
-      "albums": count2013
-    },
-    {
-      "year": 2014,
-      "albums": count2014
-    },
-    {
-      "year": 2015,
-      "albums": count2015
-    },
-    {
-      "year": 2016,
-      "albums": count2016
-    }
-  ];
+  //Finds largest and smallest year of release for any album in list, then creates an array with span of all years, and one that will hold count for albums released that year.
+  var largestYear = Math.max.apply(Math, allYears);
+  var smallestYear = Math.min.apply(Math, allYears);
+  for (var i = smallestYear; i <= largestYear; i++) {
+      yearSpan.push(i);
+  }
+  for(var i = 0; i < yearSpan.length; i++){
+  	countArray[i] = 0;
+  }
 
+  //Now counts number of albums with any particular release year...
+  for(var i = 0; i < allYears.length; i++){
+        var count = yearSpan.indexOf(allYears[i]);
+        countArray[count]++
+  }
+
+  //...and make object array
+  for(var i = 0; i < yearSpan.length; i++){
+    var yearValue = yearSpan[i];
+    var countValue = countArray[i];
+
+    item = {};
+    item ["year"] = yearValue;
+    item ["albums"] = countValue;
+
+    counts.push(item);
+  }
+
+  //Let's see if those final counts look right...
   console.log(counts);
 
   var x = d3.scale.ordinal()
@@ -158,7 +123,7 @@ d3.json('albums.json', function(err,data){
 
      yearBarText
       .attr("x", function(d){return x(d.year) + (x.rangeBand()/2)}) //Needs extra padding of half the bar width to center text
-      .attr("y", function(d){return height -y(d.albums) - 10})
+      .attr("y", function(d){return height -y(d.albums) - 10}) //-10 to float
       .attr("text-anchor", "middle")
       .attr("class", "barText")
       .attr("id", function(d){return "text" + d.year})
